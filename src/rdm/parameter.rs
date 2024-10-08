a251ffd159fd027b5529c34147b24f9e7cb0bffa
Mat <mattpowell90@gmail.com>
2024-10-09 00:58:32 +0100

    feat: added e1.37-7 parameters and handle encoding and decoding

--- src/rdm/parameter.rs
+++ src/rdm/parameter.rs
@@ -125,6 +125,23 @@ pub enum ParameterId {
     DnsIpV4NameServer,
     DnsHostName,
     DnsDomainName,
+    // E1.37-7
+    EndpointList,
+    EndpointListChange,
+    IdentifyEndpoint,
+    EndpointToUniverse,
+    EndpointMode,
+    EndpointLabel,
+    RdmTrafficEnable,
+    DiscoveryState,
+    BackgroundDiscovery,
+    EndpointTiming,
+    EndpointTimingDescription,
+    EndpointResponders,
+    EndpointResponderListChange,
+    BindingControlFields,
+    BackgroundQueuedStatusPolicy,
+    BackgroundQueuedStatusPolicyDescription,
     // E1.33
     ComponentScope,
     SearchDomain,
@@ -227,6 +244,23 @@ impl From<u16> for ParameterId {
             0x070b => Self::DnsIpV4NameServer,
             0x070c => Self::DnsHostName,
             0x070d => Self::DnsDomainName,
+            // E1.37-7
+            0x0900 => Self::EndpointList,
+            0x0901 => Self::EndpointListChange,
+            0x0902 => Self::IdentifyEndpoint,
+            0x0903 => Self::EndpointToUniverse,
+            0x0904 => Self::EndpointMode,
+            0x0905 => Self::EndpointLabel,
+            0x0906 => Self::RdmTrafficEnable,
+            0x0907 => Self::DiscoveryState,
+            0x0908 => Self::BackgroundDiscovery,
+            0x0909 => Self::EndpointTiming,
+            0x090a => Self::EndpointTimingDescription,
+            0x090b => Self::EndpointResponders,
+            0x090c => Self::EndpointResponderListChange,
+            0x090d => Self::BindingControlFields,
+            0x090e => Self::BackgroundQueuedStatusPolicy,
+            0x090f => Self::BackgroundQueuedStatusPolicyDescription,
             // E1.33
             0x8000 => Self::ComponentScope,
             0x8001 => Self::SearchDomain,
@@ -331,6 +365,23 @@ impl From<ParameterId> for u16 {
             ParameterId::DnsIpV4NameServer => 0x070b,
             ParameterId::DnsHostName => 0x070c,
             ParameterId::DnsDomainName => 0x070d,
+            // E1.37-7
+            ParameterId::EndpointList => 0x0900,
+            ParameterId::EndpointListChange => 0x0901,
+            ParameterId::IdentifyEndpoint => 0x0902,
+            ParameterId::EndpointToUniverse => 0x0903,
+            ParameterId::EndpointMode => 0x0904,
+            ParameterId::EndpointLabel => 0x0905,
+            ParameterId::RdmTrafficEnable => 0x0906,
+            ParameterId::DiscoveryState => 0x0907,
+            ParameterId::BackgroundDiscovery => 0x0908,
+            ParameterId::EndpointTiming => 0x0909,
+            ParameterId::EndpointTimingDescription => 0x090a,
+            ParameterId::EndpointResponders => 0x090b,
+            ParameterId::EndpointResponderListChange => 0x090c,
+            ParameterId::BindingControlFields => 0x090d,
+            ParameterId::BackgroundQueuedStatusPolicy => 0x090e,
+            ParameterId::BackgroundQueuedStatusPolicyDescription => 0x090f,
             // E1.33
             ParameterId::ComponentScope => 0x0800,
             ParameterId::SearchDomain => 0x0801,
@@ -2604,6 +2655,137 @@ impl TryFrom<u8> for BrokerState {
             0x01 => Ok(Self::Active),
             0x02 => Ok(Self::Standby),
             value => Err(RdmError::InvalidBrokerState(value)),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum DiscoveryState {
+    Incomplete,
+    Incremental,
+    Full,
+    NotActive,
+    ManufacturerSpecific(u8),
+}
+
+impl TryFrom<u8> for DiscoveryState {
+    type Error = RdmError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x00 => Ok(Self::Incomplete),
+            0x01 => Ok(Self::Incremental),
+            0x02 => Ok(Self::Full),
+            0x04 => Ok(Self::NotActive),
+            value if (0x80..=0xff).contains(&value) => Ok(Self::ManufacturerSpecific(value)),
+            value => Err(RdmError::InvalidDiscoveryState(value)),
+        }
+    }
+}
+
+impl From<DiscoveryState> for u8 {
+    fn from(value: DiscoveryState) -> u8 {
+        match value {
+            DiscoveryState::Incomplete => 0x00,
+            DiscoveryState::Incremental => 0x01,
+            DiscoveryState::Full => 0x02,
+            DiscoveryState::NotActive => 0x04,
+            DiscoveryState::ManufacturerSpecific(value) => value,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum DiscoveryCountStatus {
+    Incomplete,
+    Count(u16),
+    Unknown
+}
+
+impl From<u16> for DiscoveryCountStatus {
+    fn from(value: u16) -> Self {
+        match value {
+            0x0000 => Self::Incomplete,
+            0xffff => Self::Unknown,
+            value => Self::Count(value),
+        }
+    }
+}
+
+impl From<DiscoveryCountStatus> for u16 {
+    fn from(value: DiscoveryCountStatus) -> u16 {
+        match value {
+            DiscoveryCountStatus::Incomplete => 0x0000,
+            DiscoveryCountStatus::Unknown => 0xffff,
+            DiscoveryCountStatus::Count(value) => value,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum EndpointMode {
+    Disabled = 0x00, // Does not pass any DMX512-A/RDM traffic on a local RDM Command Port or DMX512-A Data Link
+    Input = 0x01, // Receives DMX512-A/RDM data on a local RDM Responder Port or DMX512-A Data Link
+    Output = 0x02, // Sends DMX512-A/RDM data on a local Command Port or DMX512-A Data Link
+}
+
+impl TryFrom<u8> for EndpointMode {
+    type Error = RdmError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x00 => Ok(Self::Disabled),
+            0x01 => Ok(Self::Input),
+            0x02 => Ok(Self::Output),
+            value => Err(RdmError::InvalidEndpointMode(value)),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum EndpointId {
+    Null,
+    Device(u16),
+    Reserved(u16),
+    Broadcast,
+}
+
+impl From<u16> for EndpointId {
+    fn from(value: u16) -> Self {
+        match value {
+            0 => EndpointId::Null,
+            0xffff => EndpointId::Broadcast,
+            value if (0xfa00..=0xfffe).contains(&value) => EndpointId::Reserved(value),
+            value => EndpointId::Device(value),
+        }
+    }
+}
+
+impl From<EndpointId> for u16 {
+    fn from(value: EndpointId) -> Self {
+        match value {
+            EndpointId::Broadcast => 0xffff,
+            EndpointId::Null => 0,
+            EndpointId::Device(value) => value,
+            EndpointId::Reserved(value) => value,
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum EndpointType {
+    Virtual = 0x00,
+    Physical = 0x01
+}
+
+impl TryFrom<u8> for EndpointType {
+    type Error = RdmError;
+
+    fn try_from(value: u8) -> Result<Self, Self::Error> {
+        match value {
+            0x00 => Ok(Self::Virtual),
+            0x01 => Ok(Self::Physical),
+            value => Err(RdmError::InvalidEndpointType(value)),
         }
     }
 }
