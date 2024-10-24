7c8d5ff6817d809f450201bfa7b3c3cd1ced6aaa
Mat <mattpowell90@gmail.com>
2024-10-24 03:23:15 +0100

    feat: impl Eq and Hash for EndpointId

--- src/rdm/parameter.rs
+++ src/rdm/parameter.rs
@@ -2742,7 +2742,7 @@ impl TryFrom<u8> for EndpointMode {
     }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub enum EndpointId {
     Null,
     Device(u16),
